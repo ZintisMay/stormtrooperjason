94dd500ebcdf4c8d82bd9c98780a2f1c696e5507
Zintis <Zintis>
2016-05-07 10:48:33 -0400

    st jason

---
+++ app/config/connection.js
@@ -0,0 +1,24 @@
+// *********************************************************************************
+// CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
+// *********************************************************************************
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    port: 3306,
+    host: 'l9dwvv6j64hlhpul.cbetxkdyhwsb.us-east-1.rds.amazonaws.com
+',
+    user: 'a0q6p8mcacwczf1v',
+    password: "prli2vul424tul6q",
+    database: "s4nrulzpgkhbdx30"
+});
+
+
+connection.connect(function(err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+    console.log('connected as id ' + connection.threadId);
+});
+
+module.exports = connection;
